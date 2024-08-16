77feb7e049eb0ff818aee75490d5f22e8a72d8a4
Matias <matiashorrisberger@gmail.com>
2024-08-16 01:05:22 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 //const {envs} = require('./config/env')
-import { envs } from './config/env.js' //ET6
+import { envs } from './config/env.js' //ES6
 
-import { startServer } from './server/server.js' //ET6
+import { startServer } from './server/server.js' //ES6
 //const { startServer } = require('./server/server')
 
 const main = () => {
