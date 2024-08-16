59aad0bfee5e7a75ef5e96926dbec71a76c39c44
Matias <matiashorrisberger@gmail.com>
2024-08-16 01:05:01 -0300

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,10 @@
-import env from 'dotenv' //ET6
-import envvar from 'env-var'; //ET6
+import env from 'dotenv' //ES6
+import envvar from 'env-var'; //ES6
 
-env.config(); //ET6
+env.config(); //ES6
 
 
-//ET6
+//ES6
 export const envs = {
     PORT: envvar.get('PORT').required().asPortNumber(),
     PUBLIC_PATH: envvar.get('PUBLIC_PATH').default('public').asString()
